df583214670867ff9d79079b7d3d432131837e4f
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <hello@sanchezcarlosjr.com>
2022-05-18 17:30:43 -0700

    upload to firebase

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
 		mainFields: ['svelte', 'browser', 'module', 'main']
 	},
 	output: {
-		path: path.join(__dirname, '/docs'),
+		path: path.join(__dirname, '/public'),
 		filename: '[name].js',
 		chunkFilename: '[name].[id].js'
 	},
