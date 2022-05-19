524f41d657214de3fc3aeb95612b54968a0e6597
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <hello@sanchezcarlosjr.com>
2022-05-18 19:37:19 -0700

    feat(algorithms):  change to string, function.toString() generate unexpected string

--- src/algorithms.js
+++ src/algorithms.js
@@ -1,4 +1,4 @@
-export function find_shortest_path_by_dijkstra(G, source_vertex) {
+export const find_shortest_path_by_dijkstra = `function find_shortest_path_by_dijkstra(G, source_vertex) {
     console.log(G);
     console.log(source_vertex);
     const path = new Set();
@@ -6,7 +6,7 @@ export function find_shortest_path_by_dijkstra(G, source_vertex) {
     while (!queue.isEmpty()) {
     }
     return path;
-}
+}`;
 
 export function build_function(algorithm) {
     return new Function("G", "source_vertex", `return ${algorithm.toString()}(G, source_vertex)`);
